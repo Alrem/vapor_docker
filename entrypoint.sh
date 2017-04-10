67b096bae07513987b93b54401d9c7790196482b
Aleksandr Kosse <aleksandr.kosse@gmail.com>
2017-04-10 12:01:10 +0300

    Add py.test ru in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [ -d vapor ]; then
   cd vapor
-  ls -l
+  . exports.sh
+  pip install -r requirements.txt
+  DATE=`date +%Y%m%d%H%M`
+  py.test -v --junit-xml=test_reports$DATE/report.xml --html=test_reports$DATE/report.html $*
 else
   echo 'Directory is not mount'
 fi
